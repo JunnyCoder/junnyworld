b52eb72f7f443599b68d3d2fd7c121840a7b13af
JunnyCoder <78329855+JunnyCoder@users.noreply.github.com>
2021-12-01 02:45:16 +0900

    맵 디비연결

--- map.php
+++ map.php
@@ -5,7 +5,19 @@
 	  <meta charset="utf-8">
 	  <title>Map Example</title>
 	  <script src="assets/js/mapdata.js"></script>
-	  <script src="assets/js/locationEditor.js"></script>
+      <?php
+      include "assets/php/DB.php";
+      $locations = DB::get()->select("SELECT * FROM map_locations ");
+      echo $locations
+      ?>
+      <script>
+          var userData =  "<?php $locations; ?>" ;
+          for( var key in userData){
+              var data = userData[key];
+              simplemaps_worldmap_mapdata["locations"] = {"0" : data[0]}
+          }
+
+      </script>
 	  <script src="assets/js/worldmap.js"></script>
 	  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	  <link rel="stylesheet" href="assets/css/main.css" />
